49d3bdf9a37a6bd08af400d021bba890adbc522d
stevenuhd <estebanprofilms@gmail.com>
2021-03-02 12:22:42 -0300

    Files upload

---
+++ src/components/Error.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Error1 = ({error}) =>{
+    return(
+<div className="error">
+    {error}
+
+</div>
+
+    )
+}
+export default Error1
---
+++ src/components/Select.js
@@ -0,0 +1,54 @@
+import React, { useState,useEffect } from "react";
+import getBreeds from '../helpers/getBreeds'
+import Error1 from "./Error";
+
+
+
+const initialBreeds = [
+{
+    id: 1,
+    name: "Boxer",
+},
+{
+    id: 2,
+    name: "Husky",
+},
+];
+
+const Select = ({updateDog}) => {
+const [breeds, setBreeds] = useState(initialBreeds);
+const[error,setError] = useState(null);
+
+useEffect(() =>{
+updateBreeds();
+},[]);
+
+const updateBreeds = () =>{
+getBreeds()
+.then((newBreeds) => {
+    setBreeds(newBreeds);
+
+})
+.catch((error) =>{
+console.log(error);
+setError('Error al cargar las razas ');
+})
+
+}
+
+return (
+    <>
+    <select onChange ={(e) => updateDog(e.target.value)}>
+        {breeds.map((breed) => (
+        <option value={breed.id} key={breed.id}>
+        {breed.name}
+        </option>
+    ))}
+    </select>
+
+    {error && <Error1 error ={error}/>}
+    </>
+);
+};
+
+export default Select;
